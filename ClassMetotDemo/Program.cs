a0b380bed7bb0af266575d27b2abb9da0a6b7f88
MEHMET FURKAN METİN <190315063@ogr.cbu.edu.tr>
2021-02-25 14:33:29 +0300

    m3 to m4

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -18,11 +18,11 @@ namespace ClassMetotDemo
             m2.Yas = 7;
             m2.ID = 1;
 
-            Musteri m3 = new Musteri();
-            m3.Isim = "Ömer";
-            m3.SoyIsim = "Metin";
-            m3.Yas = 17;
-            m3.ID = 2;
+            Musteri m4 = new Musteri();
+            m4.Isim = "Ömer";
+            m4.SoyIsim = "Metin";
+            m4.Yas = 17;
+            m4.ID = 2;
 
 
 
@@ -37,11 +37,11 @@ namespace ClassMetotDemo
             musteriManager.information(m2);
             Console.WriteLine("-------------------");
 
-            musteriManager.add(m3);
-            musteriManager.remove(m3);
-            musteriManager.information(m3);
+            musteriManager.add(m4);
+            musteriManager.remove(m4);
+            musteriManager.information(m4);
            
-
+            
 
         }
     }
